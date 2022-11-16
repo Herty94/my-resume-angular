de9eb52c843e75a935edf0ac1e49b1dc4ad2eae6
dh843qp <david.hertneky@student.tuke.sk>
2022-11-16 10:28:35 +0100

    deploy fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { TmplAstVariable } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-resume-angular';
